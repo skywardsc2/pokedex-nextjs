67214dcf58d60f6d41303c4d1103478da69baf69
Rodrigo Andrade <rodrigo.andrade@sflabs.com.br>
2022-06-04 19:42:46 -0300

    build: add MUI v5

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,19 @@
+import { ThemeProvider } from '@mui/material/styles';
+// import { ThemeProvider as Emotion10ThemeProvider } from 'emotion-theming';
+import theme from '../src/themes/theme';
+
+const withThemeProvider = (Story) => {
+  return (
+    // <Emotion10ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+    // </Emotion10ThemeProvider>
+  );
+};
+
+export const decorators = [withThemeProvider];
+
 const BREAKPOINTS_INT = {
   xs: 375,
   sm: 600,
---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,41 @@
+import Button from '@mui/material/Button';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button variant="contained" {...args}>
+    Button
+  </Button>
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  color: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  color: 'secondary',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+};
