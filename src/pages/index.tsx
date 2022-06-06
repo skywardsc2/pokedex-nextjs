3ab10929650c5971a7919d80b2713ef37f630cc9
Rodrigo Andrade <rodrigo.andrade@sflabs.com.br>
2022-06-05 23:51:54 -0300

    feat: add pokedex-promise-v2 api client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,51 @@
-import Box from '@mui/material/Box'
+import { Grid } from '@mui/material'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import type { NextPage } from 'next'
+import Pokedex, { NamedAPIResource } from 'pokedex-promise-v2'
 import * as React from 'react'
-import Link from '../components/Link'
+
+const pokedex = new Pokedex()
 
 const Home: NextPage = () => {
+  const [pokemons, setPokemons] = React.useState<NamedAPIResource[]>()
+  const [status, setStatus] = React.useState<'loading' | 'success' | 'failed'>()
+  const [errorMsg, setErrorMsg] = React.useState<unknown>()
+
+  React.useEffect(() => {
+    async function fetchPokemonList() {
+      setStatus('loading')
+      try {
+        const result = await (await pokedex.getPokemonsList({ limit: 8 })).results
+        setPokemons(result)
+        setStatus('success')
+      } catch (e: any) {
+        setStatus('failed')
+        setErrorMsg(e.msg)
+      }
+    }
+
+    fetchPokemonList()
+  }, [])
+
+  if (status === 'loading') {
+    return <Typography>Loading...</Typography>
+  }
+
+  if (status === 'failed') {
+    return <Typography>{`Error: ${errorMsg}`}</Typography>
+  }
+
   return (
-    <Container maxWidth='lg'>
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Typography variant='h4' component='h1' gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href='/' color='secondary'>
-          Go to the home page
-        </Link>
-      </Box>
+    <Container>
+      <Grid container columns={{ xs: 1, sm: 2 }}>
+        {pokemons &&
+          pokemons.map((pokemon) => (
+            <Grid item xs={1} sm={1} textAlign='center' key={pokemon.name}>
+              {pokemon.name}
+            </Grid>
+          ))}
+      </Grid>
     </Container>
   )
 }
