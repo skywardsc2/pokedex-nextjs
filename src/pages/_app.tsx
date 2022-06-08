2212f459129e7a6fabe1d7228acf8e716b48ceb2
Rodrigo Andrade <rodrigo.andrade@sflabs.com.br>
2022-06-07 22:35:33 -0300

    feat: fetch pokemon data via react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,16 @@ import { ThemeProvider } from '@mui/material/styles'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import * as React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import createEmotionCache from '../lib/createEmotionCache'
 import theme from '../themes/theme'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
+
+// React-Query client
+const queryClient = new QueryClient()
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
@@ -21,11 +26,14 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </CacheProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,107 @@
-import { Grid } from '@mui/material'
+import { Card, Grid } from '@mui/material'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import type { NextPage } from 'next'
-import Pokedex, { NamedAPIResource } from 'pokedex-promise-v2'
+import Pokedex, { Pokemon } from 'pokedex-promise-v2'
 import * as React from 'react'
+import { useQuery } from 'react-query'
 
 const pokedex = new Pokedex()
 
-const Home: NextPage = () => {
-  const [pokemons, setPokemons] = React.useState<NamedAPIResource[]>()
-  const [status, setStatus] = React.useState<'loading' | 'success' | 'failed'>()
-  const [errorMsg, setErrorMsg] = React.useState<unknown>()
+type PokemonComponentProps = { pokemon?: Pokemon } & React.ComponentPropsWithRef<'div'>
 
-  React.useEffect(() => {
-    async function fetchPokemonList() {
-      setStatus('loading')
-      try {
-        const result = await (await pokedex.getPokemonsList({ limit: 8 })).results
-        setPokemons(result)
-        setStatus('success')
-      } catch (e: any) {
-        setStatus('failed')
-        setErrorMsg(e.msg)
-      }
+const PokemonCard: React.FC<PokemonComponentProps> = ({ pokemon, ...restProps }) => {
+  if (!pokemon) {
+    return (
+      <Card
+        sx={{ display: 'flex', justifyContent: 'space-between', padding: 4 }}
+        elevation={2}
+        {...restProps}
+      >
+        <Typography variant='h3' fontWeight={'500'}>
+          Loading...
+        </Typography>
+      </Card>
+    )
+  }
+
+  return (
+    <Card
+      sx={{ display: 'flex', justifyContent: 'space-between', padding: 4 }}
+      elevation={2}
+      {...restProps}
+    >
+      <Typography variant='h3' fontWeight={'500'}>
+        {pokemon.name}
+      </Typography>
+      {pokemon.id}
+    </Card>
+  )
+}
+
+type WithPokemonDataProps = {
+  pokemonIdOrName: string | number | undefined
+  Component: React.FC<PokemonComponentProps>
+  props?: PokemonComponentProps
+}
+
+const WithPokemonData: React.FC<WithPokemonDataProps> = ({ pokemonIdOrName, Component, props }) => {
+  let {
+    data: pokemon,
+    isLoading,
+    isIdle,
+    isError,
+    error
+  } = useQuery(
+    ['get-pokemon-by-name', pokemonIdOrName],
+    async () => {
+      return pokedex.getPokemonByName(pokemonIdOrName!)
+    },
+    {
+      enabled: !!pokemonIdOrName
     }
+  )
 
-    fetchPokemonList()
-  }, [])
+  if (Array.isArray(pokemon)) {
+    pokemon = pokemon[0]
+  }
 
-  if (status === 'loading') {
+  if (isLoading || isIdle) {
     return <Typography>Loading...</Typography>
   }
 
-  if (status === 'failed') {
-    return <Typography>{`Error: ${errorMsg}`}</Typography>
+  if (isError) {
+    return <Typography>{`Error: ${error}`}</Typography>
+  }
+
+  return <Component pokemon={pokemon} {...props} />
+}
+
+const Home: NextPage = () => {
+  const {
+    data: pokemons,
+    isLoading,
+    isError,
+    error
+  } = useQuery('get-all-pokemons', async () => {
+    return pokedex.getPokemonsList({ limit: 8 })
+  })
+
+  if (isLoading) {
+    return <Typography>Loading...</Typography>
+  }
+
+  if (isError) {
+    return <Typography>{`Error: ${error}`}</Typography>
   }
 
   return (
     <Container>
-      <Grid container columns={{ xs: 1, sm: 2 }}>
+      <Grid container py={4} columns={{ xs: 1, sm: 3 }} spacing={1}>
         {pokemons &&
-          pokemons.map((pokemon) => (
+          pokemons.results.map((pokemon) => (
             <Grid item xs={1} sm={1} textAlign='center' key={pokemon.name}>
-              {pokemon.name}
+              <WithPokemonData pokemonIdOrName={pokemon.name} Component={PokemonCard} />
             </Grid>
           ))}
       </Grid>
