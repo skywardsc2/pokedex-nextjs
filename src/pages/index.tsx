a72bd04948f29058e14c6342376d8ab49ee9676b
Rodrigo Andrade <rodrigo.andrade@sflabs.com.br>
2022-06-12 21:29:11 -0300

    fix: change pokemon card design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { Card, Grid } from '@mui/material'
+import { Box, Card, Chip, Grid } from '@mui/material'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Pokedex, { Pokemon } from 'pokedex-promise-v2'
 import * as React from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
@@ -28,15 +29,37 @@ const PokemonCard: React.FC<PokemonComponentProps> = ({ pokemon, ...restProps })
   }
 
   return (
-    <Card
-      sx={{ display: 'flex', justifyContent: 'space-between', padding: 4 }}
-      elevation={2}
-      {...restProps}
-    >
-      <Typography variant='h3' fontWeight={'500'}>
-        {pokemon.name}
-      </Typography>
-      {pokemon.id}
+    <Card sx={{ padding: 2 }} elevation={2} {...restProps}>
+      <Box sx={{ display: 'grid', gridTemplateColumns: '1fr auto auto', alignItems: 'center' }}>
+        <Box sx={{ display: 'grid', gap: 1, justifyItems: 'flex-start' }}>
+          <Typography variant='h5' fontWeight={'500'}>
+            {pokemon.name}
+          </Typography>
+          <Box
+            sx={{
+              display: 'grid',
+              gap: 1,
+              gridAutoFlow: 'column',
+              justifyItems: 'flex-start',
+              justifyContent: 'flex-start'
+            }}
+          >
+            {pokemon.types &&
+              pokemon.types.map((type) => (
+                <Chip key={type.type.name} label={type.type.name} color='primary' />
+              ))}
+          </Box>
+        </Box>
+        {pokemon.sprites?.front_default && (
+          <Image
+            src={pokemon.sprites.front_default}
+            width={100}
+            height={'100%'}
+            layout='fixed'
+            alt="Pokemon's sprite"
+          />
+        )}
+      </Box>
     </Card>
   )
 }
@@ -51,16 +74,11 @@ const Home: NextPage = () => {
       })
       const pokemonsList = pokemonsPage.results
 
-      async function getPokemonDataList(
-        pokemonsList: Pokedex.NamedAPIResource[],
-        pokemonsPage: Pokedex.NamedAPIResourceList
-      ) {
-        const pokemonDataList = await Promise.all(
-          pokemonsList.map(async ({ name }) => pokedex.getPokemonByName(name))
-        )
-        return { pokemonDataList, pokemonsPage }
-      }
-      return await getPokemonDataList(pokemonsList, pokemonsPage)
+      const pokemonDataList = await Promise.all(
+        pokemonsList.map(async ({ name }) => pokedex.getPokemonByName(name))
+      )
+
+      return { pokemonDataList, pokemonsPage }
     },
     {
       getNextPageParam: (lastPage, pages) => {
@@ -88,7 +106,8 @@ const Home: NextPage = () => {
         loader={<Typography variant='h4'>Loading...</Typography>}
         height={800}
         style={{
-          width: '100%'
+          width: '100%',
+          height: '100vh'
         }}
       >
         <Grid container py={4} columns={{ xs: 1, sm: 3 }} spacing={1}>
