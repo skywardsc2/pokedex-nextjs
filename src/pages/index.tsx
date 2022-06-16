f3626bd2683ee07dda0b5eca029eb3e19cb2f02e
Rodrigo Andrade <rodrigo.andrade@sflabs.com.br>
2022-06-16 15:11:45 -0300

    fix: fix page performance
    add pokemon type listing and decrease data load amount
    disable eslint rule no-undef for ts files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
-import { Box, Card, Chip, Grid } from '@mui/material'
+import { Box, Card, Chip, Grid, styled } from '@mui/material'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import Pokedex, { Pokemon } from 'pokedex-promise-v2'
 import * as React from 'react'
+
+import type { GetStaticProps, NextPage } from 'next'
+import Image from 'next/image'
+
+import Pokedex, { NamedAPIResourceList, Pokemon } from 'pokedex-promise-v2'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useInfiniteQuery } from 'react-query'
+
+const StyledInfiniteScroll = styled(InfiniteScroll)``
 
 const pokedex = new Pokedex()
 const POKEMONS_PER_PAGE = 20
 
-type PokemonComponentProps = { pokemon?: Pokemon } & React.ComponentPropsWithRef<'div'>
+type PokemonComponentProps = { pokemon?: PokemonShortData } & React.ComponentPropsWithRef<'div'>
 
 const PokemonCard: React.FC<PokemonComponentProps> = ({ pokemon, ...restProps }) => {
   if (!pokemon) {
@@ -50,9 +54,9 @@ const PokemonCard: React.FC<PokemonComponentProps> = ({ pokemon, ...restProps })
               ))}
           </Box>
         </Box>
-        {pokemon.sprites?.front_default && (
+        {pokemon.sprite && (
           <Image
-            src={pokemon.sprites.front_default}
+            src={pokemon.sprite}
             width={100}
             height={'100%'}
             layout='fixed'
@@ -64,25 +68,82 @@ const PokemonCard: React.FC<PokemonComponentProps> = ({ pokemon, ...restProps })
   )
 }
 
-const Home: NextPage = () => {
+type PokemonShortData = Pick<Pokemon, 'name' | 'id' | 'types'> & {
+  sprite: string | null
+}
+
+type PokemonShortDataPage = Omit<NamedAPIResourceList, 'results'> & {
+  results: PokemonShortData[]
+}
+
+interface PageProps {
+  initialPokemonShortDataPage: PokemonShortDataPage
+  pokemonTypeList: { name: string }[]
+  pokemonAbilityList: { name: string }[]
+}
+
+const MapPokemonData = (pokemonData: PokemonShortData): JSX.Element => {
+  return (
+    <Grid item xs={1} sm={1} textAlign='center' key={pokemonData?.name}>
+      <PokemonCard pokemon={pokemonData} />
+    </Grid>
+  )
+}
+
+const MapPokemonDataPage = (pokemonDataPage: PokemonShortDataPage): JSX.Element[] => {
+  return pokemonDataPage.results.map(MapPokemonData)
+}
+
+async function fetchPokemonShortDataPage({
+  limit = POKEMONS_PER_PAGE,
+  offset = 0
+}): Promise<PokemonShortDataPage> {
+  const pokemonList = await pokedex.getPokemonsList({
+    limit,
+    offset
+  })
+
+  const pokemonDataList = await Promise.all(
+    pokemonList.results.map(async ({ name }) => {
+      const pokemonOrList = await pokedex.getPokemonByName(name)
+      if (Array.isArray(pokemonOrList)) return pokemonOrList[0]
+      return pokemonOrList
+    })
+  )
+
+  // Filter pokemon data for the preview cards
+  const pokemonShortDataPage: PokemonShortDataPage = {
+    ...pokemonList,
+    results: pokemonDataList.map(({ name, id, types, sprites }) => ({
+      name,
+      id,
+      types,
+      sprite: sprites?.front_default
+    }))
+  }
+
+  return pokemonShortDataPage
+}
+
+const Home: NextPage<PageProps> = ({
+  initialPokemonShortDataPage: initialPokemonDataPage,
+  pokemonTypeList
+  // pokemonAbilityList
+}) => {
   const { data, isLoading, isError, error, fetchNextPage, hasNextPage } = useInfiniteQuery(
     'infinite-get-all-pokemons',
-    async ({ pageParam = 0 }) => {
-      const pokemonsPage = await pokedex.getPokemonsList({
+    async ({ pageParam = 0 }) =>
+      fetchPokemonShortDataPage({
         limit: POKEMONS_PER_PAGE,
         offset: pageParam * POKEMONS_PER_PAGE
-      })
-      const pokemonsList = pokemonsPage.results
-
-      const pokemonDataList = await Promise.all(
-        pokemonsList.map(async ({ name }) => pokedex.getPokemonByName(name))
-      )
-
-      return { pokemonDataList, pokemonsPage }
-    },
+      }),
     {
+      initialData: {
+        pages: [initialPokemonDataPage],
+        pageParams: [0]
+      },
       getNextPageParam: (lastPage, pages) => {
-        if (lastPage.pokemonsPage.next) {
+        if (lastPage.next) {
           return pages.length + 1
         }
       }
@@ -98,37 +159,52 @@ const Home: NextPage = () => {
   }
 
   return (
-    <Container>
-      <InfiniteScroll
+    <Container sx={{ paddingTop: 4 }}>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', padding: '1 0' }}>
+        {pokemonTypeList.map((type) => (
+          <Chip label={type.name} clickable key={type.name} />
+        ))}
+      </Box>
+      <StyledInfiniteScroll
         dataLength={(data?.pages.length || 0) * POKEMONS_PER_PAGE}
         next={fetchNextPage}
         hasMore={!!hasNextPage}
         loader={<Typography variant='h4'>Loading...</Typography>}
         height={800}
-        style={{
+        sx={{
           width: '100%',
-          height: '100vh'
+          height: '100vh',
+          padding: 2
         }}
       >
         <Grid container py={4} columns={{ xs: 1, sm: 3 }} spacing={1}>
-          {data?.pages.map(({ pokemonDataList }) => {
-            return pokemonDataList.map((pokemonData) => {
-              let pokemon
-
-              if (Array.isArray(pokemonData)) pokemon = pokemonData[0]
-              else pokemon = pokemonData
-
-              return (
-                <Grid item xs={1} sm={1} textAlign='center' key={pokemon?.name}>
-                  <PokemonCard pokemon={pokemon} />
-                </Grid>
-              )
-            })
-          })}
+          {data?.pages.map(MapPokemonDataPage)}
         </Grid>
-      </InfiniteScroll>
+      </StyledInfiniteScroll>
     </Container>
   )
+}
+
+export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const initialPokemonShortDataPage: PokemonShortDataPage = await fetchPokemonShortDataPage({
+    limit: POKEMONS_PER_PAGE
+  })
+
+  const pokemonTypeList = (await pokedex.getTypesList()).results.map((resource) => ({
+    name: resource.name
+  }))
+
+  const pokemonAbilityList = (await pokedex.getAbilitiesList()).results.map((resource) => ({
+    name: resource.name
+  }))
+
+  return {
+    props: {
+      initialPokemonShortDataPage: initialPokemonShortDataPage,
+      pokemonTypeList: pokemonTypeList,
+      pokemonAbilityList: pokemonAbilityList
+    }
+  }
 }
 
 export default Home
